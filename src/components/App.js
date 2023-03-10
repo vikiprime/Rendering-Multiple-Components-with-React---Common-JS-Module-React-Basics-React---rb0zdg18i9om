ce182969d572f0fea909d0e371742e565418fe5f
Akshay Ashok Giri <114691327+vikiprime@users.noreply.github.com>
2023-03-10 14:19:23 +0530

    project-name

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test = "project-name">MOVIE BOOKING WEBSITE</h1>
+      <P data-ns-test = "project-description">This is the project about movie booking website and its navigation</P>
+    </div>
   )
 }
 
