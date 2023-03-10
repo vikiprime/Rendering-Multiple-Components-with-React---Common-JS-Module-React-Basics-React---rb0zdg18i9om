2b8860c4d70dc3dbecfe70df2ad70b3c754bca4c
Akshay Ashok Giri <114691327+vikiprime@users.noreply.github.com>
2023-03-10 15:05:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
       <h1 data-ns-test = "project-name">MOVIE BOOKING WEBSITE</h1>
-      <P data-ns-test = "project-description">This is the project about movie booking website and its navigation</P>
+      <p data-ns-test = "project-description">This is the project about movie booking website and its navigation</P>
     </div>
   )
 }
